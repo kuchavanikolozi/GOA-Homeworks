75acd97215145302e27fe3dd870b80e01169ff7a
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-09-08 22:53:07 +0400

    first commit

---
+++ day 30/Classwork/მათემატიკური ოპერატორები ფუნქციებით/classworkday30.py
@@ -0,0 +1,15 @@
+# გაყოფა
+def division(a, b):
+    return a / b
+
+# გამრავლება
+def multiplication(a, b):
+    return a * b
+
+# მიმატება
+def addition(a, b):
+    return a + b
+
+#  გამოკლება
+def subtraction(a, b):
+    return a - b
---
+++ day 30/Classwork/რიცხვი არის თუ არა 10-ზე მეტი/classworkday30.py
@@ -0,0 +1,6 @@
+def check_number():
+    number = int(input("Please input an number: "))
+    if number > 10:
+        print("Number is more than 10.")
+    else:
+        print("Number is less than 10 or =.")
