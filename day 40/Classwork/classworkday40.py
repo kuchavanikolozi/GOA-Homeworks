d9c24b6cce59230853ad52ba6b28f4869d704e65
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-11-03 21:43:44 +0400

    commit

---
+++ day 40/Classwork/classworkday40.py
@@ -0,0 +1,23 @@
+# codewars
+
+# 1)
+
+# This code does not execute properly. Try to figure out why.
+
+# def multiply(a, b):
+    # a * b
+
+# correct code is:
+
+def multiply(a, b):
+    return a * b
+
+# 2) 
+
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
