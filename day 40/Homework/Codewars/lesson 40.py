451467945434d4b5ba13da213c4b6b171cab1410
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-11-17 13:57:35 +0400

    commit

---
+++ day 40/Homework/Codewars/lesson 40.py
@@ -0,0 +1,20 @@
+# 1)
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return 'Even'
+#     else: 
+#         return 'Odd'
+
+# 2)   
+# This code does not execute properly. Try to figure out why.
+# def multiply(a, b):
+#       a * b
+
+# code output error because it needs 'return'
+
+# correct code:
+
+# def multiply(a, b):
+    # return a * b
---
+++ day 40/Homework/sandwich_maker და greet ფუნქცია/lesson 40.py
@@ -0,0 +1,17 @@
+# ფუნქცია რომელიც მომხმარებელს მიესალმება
+
+# def greet(name):
+#     print(f"Hello, {name}!")
+# greet("Nikolozi")
+
+# sandwith_make ფუნქცია რომლების არგუმენტების დახმარებით გავაკეთებთ თქვენს ბუტერბროტს
+
+# def sandwich_make(bread, meat, vegetables, sauce):
+#     print(f"თქვენი ბუტერბროტი შედგება შემდეგი ინგრედიენტებისგან:")
+#     print(f"- პური: {bread}")
+#     print(f"- ხორცი: {meat}")
+#     print(f"- ბოსტნეული: {vegetables}")
+#     print(f"- სოუსი: {sauce}")
+#     print("გემრიელად მიირთვით")
+
+# sandwich_make("თეთრი პური", "ქათმის ფილე", "პომიდორი, კიტრი, სალათის ფურცელი", "ტერიაკი")
